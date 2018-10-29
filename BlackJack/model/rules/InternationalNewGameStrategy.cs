3d777dc7bf9731f2a8c9fe91de3822241c6de464
Yasmin <ym222bs@student.lnu.se>
2018-10-29 22:20:50 +0100

    done3

--- BlackJack/model/rules/InternationalNewGameStrategy.cs
+++ BlackJack/model/rules/InternationalNewGameStrategy.cs
@@ -11,9 +11,9 @@ namespace model.rules
         public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
             
-            a_dealer.DealCardFromDeck(true,  a_player);
-            a_dealer.DealCardFromDeck(true,  a_dealer);
-            a_dealer.DealCardFromDeck(true,  a_player);
+            a_dealer.DealCardFromDeck(true, a_player);
+            a_dealer.DealCardFromDeck(true, a_dealer);
+            a_dealer.DealCardFromDeck(true, a_player);
 
             return true;
         }
